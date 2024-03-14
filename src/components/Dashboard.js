919dc6e9d7bc573a4abd74d2efd9383d76144bd0
shushinde111@gmail.com <shushinde111@gmail.com>
2024-03-14 17:42:56 +0530

    old build removed

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,7 +3,17 @@ import React from "react";
 export default function dashboard() {
   return (
     <div className="">
-      <div className="w-3/4 bg-white p-4 rounded-md">Hello Shubham</div>
+      <div className="flex gap-2">
+        <div className="w-3/4 flex flex-col gap-2">
+          <div className="  bg-white p-4 rounded-md">
+            <h2></h2>
+          </div>
+          <div className=" h-96 bg-white p-4 rounded-md">
+            Good Morning Shubham
+          </div>
+        </div>
+        <div className="w-1/4 bg-white p-4 rounded-md">calender</div>
+      </div>
     </div>
   );
 }
