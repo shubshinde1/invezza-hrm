a65f0228e71b23992fc97734dc54fae0df37bfde
shushinde111@gmail.com <shushinde111@gmail.com>
2024-03-19 22:53:43 +0530

    cleanup

--- src/components/Myprofile.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function myprofile() {
-  return <div>My profile</div>;
-}
--- src/components/Task.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Task() {
-  return <div>tasks</div>;
-}
